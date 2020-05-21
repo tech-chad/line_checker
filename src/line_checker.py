902b17ddb99f07911afae496bc74920fdb86bf7c
Chad Larson <techchad2@gmail.com>
2020-05-20 23:48:20 -0500

    Worked on the output

--- src/line_checker.py
+++ src/line_checker.py
@@ -1,11 +1,14 @@
 """ Line length checker. """
 import argparse
+import shutil
+
 
 from typing import List
 from typing import Tuple
 
 
 DEFAULT_LINE_LENGTH = 80
+SEP = "-"
 
 
 class LineCheckerError(Exception):
@@ -24,8 +27,8 @@ def load_file(filename: str) -> List[str]:
     try:
         with open(filename, "r") as f:
             data = f.read()
-    except FileNotFoundError:
-        raise FileNotFoundError
+    except FileNotFoundError as e:
+        raise FileNotFoundError(e)
 
     line_data = []
     for d in data.splitlines():
@@ -42,36 +45,51 @@ def checker(line_data: List[str]) -> List[Tuple[int, int]]:
     return fail_lines
 
 
-def build_summary(filename: str,
-                  file_failed: str,
-                  fail_lines: List[Tuple[int, int]]) -> str:
+def display_check_completed() -> None:
+    terminal_col, _ = shutil.get_terminal_size()
+    title = "1 file checked in 0.01 seconds"
+    title_sep = SEP * int((terminal_col/2) - 1 - len(title)/2)
+    completed_line = f"{title_sep} {title} {title_sep}"
+    print(completed_line)
 
-    summary = f"""
--------- Check completed --------
-{filename}...[{file_failed}]
 
-"""
-    fail_summary = "Lines     length\n"
-    for line in fail_lines:
-        fail_summary += f"{line[0] + 1}         {line[1]}\n"
+def display_summary(filename: str,
+                    file_failed: str,
+                    fail_lines: List[Tuple[int, int]]) -> None:
+    dot = "."
+    terminal_col, _ = shutil.get_terminal_size()
+    # title = "Summary"
+    # title_sep = SEP * int((terminal_col/2) - 1 - len(title)/2)
+    # summary_line = f"{title_sep} {title} {title_sep}"
 
-    summary += fail_summary
-    summary += "------ END SUMMARY ------"
-    return summary
+    dot_fill = dot * (terminal_col - len(filename) - len(file_failed) - 5)
+    file_line = f"{filename}{dot_fill}[{file_failed}]"
+
+    print()
+    # print(summary_line)
+    print(file_line)
+    if fail_lines:
+        print("      Line     Length")
+        for line in fail_lines:
+            print(f"      {line[0] + 1}        {line[1]}")
 
 
 def display_welcome() -> None:
-    print("-------- Line Checker --------")
-    print(f"          version")  # TODO add version here
+    # use _ for terminal_lines
+    terminal_col, terminal_lines = shutil.get_terminal_size()
+    title = "Line Checker"
+    title_sep = SEP * int((terminal_col/2) - 1 - len(title)/2)
+    title_line = f"{title_sep} {title} {title_sep}"
+
+    print(title_line)
+    print("version: 0.0.0")
+    print()
 
 
-def display_error() -> None:
-    print("Error file not found or file is not a python file")
+def display_error(error_message) -> None:
+    print(f"Error: {error_message}")
+    print()
     print("Aborting")
-
-
-def display_summary(summary: str) -> None:
-    print(summary)
 
 
 def argument_parsing(argv: list = None) -> argparse.Namespace:
@@ -87,8 +105,8 @@ def main(argv: list = None) -> int:
     if verify_filename(args.file):
         try:
             file_data = load_file(args.file)
-        except FileNotFoundError as e:
-            print(e)
+        except FileNotFoundError:
+            display_error("File is not found")
             return 1
         else:
             fail_lines = checker(file_data)
@@ -96,12 +114,14 @@ def main(argv: list = None) -> int:
                 file_failed = "FAIL"
             else:
                 file_failed = "PASSED"
-            summary = build_summary(args.file, file_failed, fail_lines)
-            display_summary(summary)
+
+            display_check_completed()
+            display_summary(args.file, file_failed, fail_lines)
             return 0
 
     else:
-        display_error()
+        display_error("File is not "
+                      "a check-able file")
         return 1
 
 
